0922c5eb1f2cbf91be385e94143456a79f67f39a
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-08-28 09:34:41 -0700

    Adds readme

---
+++ README.md
@@ -0,0 +1,9 @@
+###Welcome to version 2048 6.3.2beta
+##How to Play
+* Two players enter......both players also potentially leave, one slightly more high spirited than the other after this, the greatest strategic game ever made!!! ...by me.
+* Anyways, tiles slide towards different directions based on input.
+* Player 1 uses arrow keys
+* Player two uses a, s, d, w keys
+* Tiles only add with tiles with equal values.
+* First player to reach 2567241678 total score wins.
+* Don't die.
--- gameBoard.js
+++ gameBoard.js
@@ -1,30 +1,38 @@
-var gameBoard = [ [ 0, 0, 0, 0 ],
-				  [ 0, 0, 0, 0 ],
-				  [ 0, 0, 0, 0 ],
-				  [ 0, 0, 0, 0 ] ];
+// var gameBoard = [ [ 0, 0, 0, 0 ],
+// 				  [ 0, 0, 0, 0 ],
+// 				  [ 0, 0, 0, 0 ],
+// 				  [ 0, 0, 0, 0 ] ];
 
-function printBoard( board ) {
-	var strBoard = "";
-	for ( var i = 0; i < board.length; i++ ) {
-		strBoard = "";
-		for ( var j = 0; j < board[ i ].length; j++ ) {
-			strBoard +=  board[ i ][ j ] + " ";
-		}
-		console.log( strBoard );
-	}
-}
+// function printBoard( board ) {
+// 	var strBoard = "";
+// 	for ( var i = 0; i < board.length; i++ ) {
+// 		strBoard = "";
+// 		for ( var j = 0; j < board[ i ].length; j++ ) {
+// 			strBoard +=  board[ i ][ j ] + " ";
+// 		}
+// 		console.log( strBoard );
+// 	}
+// }
 
-function randomAddTopBottom( board, row ) {
-    board[ row ][ Math.floor( Math.random() * 4 ) ] = 2;
-    return board;
-}
+// function randomAddTopBottom( board, row ) {
+//     board[ row ][ Math.floor( Math.random() * 4 ) ] = 2;
+//     return board;
+// }
 
-function randomAddLeftRight( board, col ) {
-	board[ Math.floor( Math.random() * 4 ) ][ col ] = 2;
-	return board;
-}
+// function randomAddLeftRight( board, col ) {
+// 	board[ Math.floor( Math.random() * 4 ) ][ col ] = 2;
+// 	return board;
+// }
 
 
-function numberZeroOrThree() {
-   return ( ( Math.floor( Math.random() * 100 ) > 50 ) ? 3 : 0 );
-}
+// function numberZeroOrThree() {
+//    return ( ( Math.floor( Math.random() * 100 ) > 50 ) ? 3 : 0 );
+// }
+
+var $gameBoardArray = $( ".box" );
+
+$.each( $gameBoardArray, function() {
+	this.click( function() {
+		console.log( this );
+	});
+}) 
